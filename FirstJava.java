a7d182e5ea8fac495640f3c08bf2c42f69f84260
James Chaney <42975010+jmz007@users.noreply.github.com>
2019-05-04 11:11:57 -0500

    Add files via upload

---
+++ FirstJava.java
@@ -0,0 +1,7 @@
+public class FirstJava {
+    public static void main(String[] args) {
+        System.out.println("My name is Bond, James Bond");
+        System.out.println("I am too old");
+        System.out.println("My hometown is a state secret");
+    }
+}
